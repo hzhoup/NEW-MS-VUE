b955e8bc838bff7e7db0b462d0f1f52e7a8790af
hzp <hzhouping@outlook.com>
2022-10-28 22:08:35 +0800

    feat: 集成 pinia

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
 import './styles/common.scss'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
