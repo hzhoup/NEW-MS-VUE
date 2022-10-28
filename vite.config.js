16af1e440aa74d11f43064bc71b6ed568215cf42
hzp <hzhouping@outlook.com>
2022-10-28 22:39:49 +0800

    feat: 按需加载 antd vue

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,46 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
+import { AndDesignVueResolve, createStyleImportPlugin } from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia'],
+      eslintrc: {
+        enabled: true,
+        filepath: './.eslintrc-auto-import.json',
+        globalsPropValue: true
+      }
+    }),
+    Components({
+      resolvers: [AntDesignVueResolver({ resolveIcons: true })]
+    }),
+    createStyleImportPlugin({
+      resolves: [AndDesignVueResolve()],
+      libs: [
+        {
+          libraryName: 'ant-design-vue',
+          esModule: true,
+          resolveStyle: (name) => `ant-design-vue/es/${name}/style/index`
+        }
+      ],
+      css: {
+        preprocessorOptions: {
+          less: {
+            javascriptEnabled: true
+          }
+        }
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
@@ -20,7 +55,7 @@ export default defineConfig({
         target: 'http://localhost:9001',
         changeOrigin: true,
         secure: false,
-        rewrite: path => path.replace('/api/', '/api/')
+        rewrite: (path) => path.replace('/api/', '/api/')
       }
     }
   }
